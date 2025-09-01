4568f44bd607e986cb11ba3e0bf3a5acb59c39c3
Fantasy <1362724990@qq.com>
2025-09-01 14:36:20 +0800

    feat: 根据项目定制和精简初始化模板

---
+++ rabbitdada-backend/src/main/java/com/fantasy/rabbitdada/controller/PostThumbController.java
@@ -0,0 +1,53 @@
+package com.fantasy.rabbitdada.controller;
+
+import com.fantasy.rabbitdada.common.BaseResponse;
+import com.fantasy.rabbitdada.common.ErrorCode;
+import com.fantasy.rabbitdada.common.ResultUtils;
+import com.fantasy.rabbitdada.exception.BusinessException;
+import com.fantasy.rabbitdada.model.dto.postthumb.PostThumbAddRequest;
+import com.fantasy.rabbitdada.model.entity.User;
+import com.fantasy.rabbitdada.service.PostThumbService;
+import com.fantasy.rabbitdada.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ rabbitdada-backend/src/main/java/com/fantasy/rabbitdada/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.fantasy.rabbitdada.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fantasy.rabbitdada.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
